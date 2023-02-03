485fc1b7600244ac881c71589faf0c835e95cd2e
marin <marin@marinaPC>
2023-02-03 19:36:13 +0900

    Review03完了

---
+++ src/baseball/BaseBallTeam.java
@@ -0,0 +1,61 @@
+package baseball;
+
+public class BaseBallTeam {
+    // フィールド
+    private String name; // 球団名
+    private int win;     // 勝ち
+    private int lose;    // 負け
+    private int draw;    //　引き分け
+
+    // 引数なしのコンストラクタ
+    public BaseBallTeam() {
+    }
+
+    //　引用ありのコンストラクタ
+    public BaseBallTeam(String name,int win,int lose,int draw) {
+        this.name = name;
+        this.win = win;
+        this.lose = lose;
+        this.draw = draw;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getwin() {
+        return win;
+    }
+
+    public void setWin(int win) {
+        this.win = win;
+    }
+
+    public int getlose() {
+        return lose;
+    }
+
+    public void setLose(int lose) {
+        this.lose = lose;
+    }
+
+    public  int getdraw() {
+        return draw;
+    }
+
+    public void setDraw(int draw) {
+        this.draw = draw;
+    }
+
+    public double getRate() {
+        return (double) this.win/(this.win + this.lose);
+    }
+
+    public void report() {
+        System.out.println(this.name + "の2022年の成績は" + this.win + "勝" + this.lose + "敗" + this.draw + "分、勝率は" + getRate()+ "です。");;
+    }
+}
